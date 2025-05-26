aa5052ae68145ca5c1925116663fd0d83ed6a75f
Paulo Ricka <rick.ateixeira@hotmail.com>
2025-05-26 13:30:01 -0300

    implementa CRUD completo de empresas (GET, POST, PUT, DELETE)

--- app.js
+++ app.js
@@ -9,40 +9,64 @@ server.get('/api/company', async (request, reply) => {
   return result.rows
 })
 
-server.post('/api/company', async (request, reply) => {
-    const {nome, descricao, active} = request.body
-
-    database.create({
-        nome,
-        descricao,
-        active,
-    })
-
-    return reply.status(201).send()
-    })
 
 server.put('/api/company/:id', async (request, reply) => {
-    const companyId = request.params.id
-    const {nome, descricao, active} = request.body
+  const companyId = request.params.id
+  const {
+    avatarUrl,
+    nomeFantasia,
+    razaoSocial,
+    qtdeFuncionarios
+  } = request.body
 
+  await db.query(
+    `UPDATE companies
+     SET "avatarUrl" = $1,
+         "nomeFantasia" = $2,
+         "razaoSocial" = $3,
+         "qtdeFuncionarios" = $4
+     WHERE id = $5`,
+    [avatarUrl, nomeFantasia, razaoSocial, qtdeFuncionarios, companyId]
+  )
 
-    database.uptade(companyId, {
-        nome,
-        descricao,
-        active,
-
-    })
-
-    return reply.status(204).send()
-    
+  return reply.status(204).send()
 })
 
-server.delete('/api/company/:id', async (request, reply) => {
-    const companyId = request.params.id
-    database.delete(companyId)
 
-    return reply.status(204).send()
-    
+server.post('/api/company', async (request, reply) => {
+  const {
+    avatarUrl,
+    nomeFantasia,
+    razaoSocial,
+    qtdeFuncionarios
+  } = request.body;
+
+ 
+  const maxId = await db.query('SELECT MAX(id) FROM companies')
+  const nextId = (maxId.rows[0].max || 0) + 1;
+  
+  await db.query(`ALTER SEQUENCE companies_id_seq RESTART WITH ${nextId}`)
+  
+  const result = await db.query(
+    `INSERT INTO companies ("avatarUrl", "nomeFantasia", "razaoSocial", "qtdeFuncionarios", active)
+     VALUES ($1, $2, $3, $4, TRUE)
+     RETURNING *`,
+    [avatarUrl, nomeFantasia, razaoSocial, qtdeFuncionarios]
+  );
+
+  return reply.status(201).send(result.rows[0])
+});
+
+
+server.delete('/api/company/:id', async (request, reply) => {
+  const companyId = request.params.id
+
+  await db.query(
+    `UPDATE companies SET active = FALSE WHERE id = $1`,
+    [companyId]
+  )
+
+  return reply.status(204).send()
 })
 
 
